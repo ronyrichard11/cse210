144704d859cd22f29aacb980562b3927ca58245c
Rony Richard <rony.richard11@gmail.com>
2023-01-11 18:36:03 -0500

    Final C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 11);
+
+        Console.WriteLine($"The Magic Number is {magic_number}.");
+        int guess = -1;
+
+        while (guess != magic_number){
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magic_number){
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magic_number){
+                Console.WriteLine("Higher");
+            }
+            
+        }
+        if (guess == magic_number){
+            Console.WriteLine("You Guessed it!");
+        }
     }
 }
