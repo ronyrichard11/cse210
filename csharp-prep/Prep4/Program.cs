764e0eac01efdc242ee3827e535a6dbcb619c0a3
Rony Richard <rony.richard11@gmail.com>
2023-01-11 19:09:55 -0500

    Final Product

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> num_list = new List<int>();
+
+        int number = -1;
+        int sum = 0;
+        
+
+        Console.WriteLine("Enter a List of Numbers, type 0 when finished!");
+
+        while (number != 0){
+            Console.WriteLine("Enter Number:");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0){
+                num_list.Add(number);
+            }
+        }
+
+        foreach (int i in num_list)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine($"The sum is {sum}");
+
+        float average = ((float)sum) / num_list.Count;
+        Console.WriteLine($"The average is {average}");
+
+        int largest = num_list[0];
+
+        foreach (int numbers in num_list)
+        {
+            if (numbers > largest)
+            {
+
+                largest = numbers;
+            }
+        }
+
+        Console.WriteLine($"The max is {largest}");
+
     }
 }
