15ece390c4dc7e6f32b937f4db81b98b02f1873b
Rony Richard <rony.richard11@gmail.com>
2023-01-18 21:21:22 -0500

    Step 4 Complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Daves Chicken";
+        job1._jobTitle = "Executive Marketing Promoter";
+        job1._startYear = 2024;
+        job1._endYear = 2027;
+
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._company = "Applebees";
+        job2._jobTitle = "Underwater Ceramics Technician";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Console.WriteLine(job2._company);
     }
 }
